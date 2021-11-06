c2ec638a47f72a68c2b8c679c70e5a0023298d07
SandraQS <sanquesan@gmail.com>
2021-11-06 20:09:40 +0100

    Add info api at app.js

--- src/components/Robot/Robot.js
+++ src/components/Robot/Robot.js
@@ -1,18 +1,26 @@
-const Robot = () => {
+const Robot = ({ infoRobot }) => {
   return (
     <article className="card-robot">
-      <h2 className=" card-robot__title">nombre robot</h2>
       <img
-        src="url robot"
-        alt="nombre robot"
+        src={infoRobot.imagen}
+        alt={infoRobot.nombre}
         className="card-robot__image"
         whith="150"
       />
-      <h3 className="card-robot__title-features">Características:</h3>
+      <h2 className=" card-robot__title">{infoRobot.nombre}</h2>
+      <h3 className="card-robot__title-features">
+        Características de {infoRobot.nombre}
+      </h3>
       <ul className="card-robot__list-features">
-        <li className="card-robot__feature">Velocidad: </li>
-        <li className="card-robot__feature">Resistencia: </li>
-        <li className="card-robot__feature">Fecha de creación: </li>
+        <li className="card-robot__feature">
+          Velocidad: {infoRobot.caracteristicas.velocidad}
+        </li>
+        <li className="card-robot__feature">
+          Resistencia: {infoRobot.caracteristicas.resistencia}
+        </li>
+        <li className="card-robot__feature">
+          Fecha de creación: {infoRobot.caracteristicas.creacion}
+        </li>
       </ul>
       <button type="button" className="btn btn-dark card-robot__edit">
         Editar
