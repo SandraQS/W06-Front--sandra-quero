2d0ae86dcae7a00c8ff37a86132385a4fec7f5d6
SandraQS <sanquesan@gmail.com>
2021-11-07 12:25:56 +0100

    Created test snapshots for Robot component

---
+++ LISTA_RESPONSABILIDADES-md
@@ -0,0 +1,21 @@
+LISTA ROBOTS:
+
+- RECIBE un array de objetos.
+- Renderiza una lista con el componente Robot.
+- Pasa los datos a Robot.
+
+ROBOTS:
+
+- RECIBE un objeto robot con su información.
+- Renderiza una imagen, un h2 con el nombre, un h3 con un titulo, y una lista de caracteríscias.
+- Renderiza un boton ELMINIAR.
+  - Ejecuta una funcion RECIBIDA al hacer click.
+- Renderiza un boton EDITAR.
+  - Ejecuta una función RECIBIDA al hacer click.
+
+FORM:
+
+- Renderiza un form con inputs type text.
+  - Almacena y envia los datos introducidos en los input.
+- Renderica un botón CREAR.
+  - Ejecuta una funcion RECIBIDA al hacer click.
---
+++ src/components/Robot/Robots.test.js
@@ -0,0 +1,26 @@
+import ReactTestRenderer from "react-test-renderer";
+import Robot from "./Robot";
+
+describe("Given Robots component", () => {
+  describe("When it's rendered", () => {
+    test("Then it should match with de last snapshot", () => {
+      const robot = {
+        caracteristicas: {
+          velocidad: 5,
+          resistencia: 9,
+          creacion: "1996-05-19T22:00:00.000Z",
+        },
+        _id: "61855ad154ce63991d588ddb",
+        nombre: "Aire",
+        imagen:
+          "https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/styles/hc_1440x810/public/media/image/2020/04/astro-robot-ps5-1920415.jpg?h=d1cb525d&itok=NpaKRK-Q",
+      };
+
+      const robotCard = ReactTestRenderer.create(
+        <Robot infoRobot={robot} key={robot._id} />
+      );
+
+      expect(robotCard.toJSON()).toMatchSnapshot();
+    });
+  });
+});
