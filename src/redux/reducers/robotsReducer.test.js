81b2bc09603120aeeb66e176d227130466847799
SandraQS <sanquesan@gmail.com>
2021-11-07 23:47:38 +0100

    Created test for case deleteRobotById at robotReducer reducer

--- src/redux/reducers/robotsReducer.test.js
+++ src/redux/reducers/robotsReducer.test.js
@@ -2,11 +2,12 @@ import {
   loadRobotByIdAction,
   loadRobotsAction,
   createRobotAction,
+  deleteRobotByIdAction,
 } from "../actions/actionCreators";
 import robotsReducer from "./robotsReducer";
 
 describe("Given robotsReducer reducer", () => {
-  describe("When it receives an array and action loadRobots", () => {
+  describe("When it receives an array and action loadRobotsAction", () => {
     test("Then it should return a new array of objects", () => {
       const arrayRobots = [
         {
@@ -41,7 +42,7 @@ describe("Given robotsReducer reducer", () => {
     });
   });
 
-  describe("When it receives an array and action loadRobotById, with id", () => {
+  describe("When it receives an array and action loadRobotByIdAction, with id", () => {
     test("Then it should return a new array with an object with the proper id", () => {
       const id = "61855ad154ce63991d588ddb";
       const action = loadRobotByIdAction(id);
@@ -100,7 +101,7 @@ describe("Given robotsReducer reducer", () => {
     });
   });
 
-  describe("When it receives an array and object and an createRobot action with a new object", () => {
+  describe("When it receives an array and object and an createRobotAction action with a new object", () => {
     test("Then it should return an array with this object added", () => {
       const newRobot = {
         _id: "61857c0154ce63991d588ddc",
@@ -131,6 +132,52 @@ describe("Given robotsReducer reducer", () => {
       const newRobots = robotsReducer(robots, action);
 
       expect(newRobots).toContain(newRobot);
+    });
+  });
+
+  describe("When it receives an array and object and an deleteRobotByIdAction action with an id", () => {
+    test("Then it should return an array without the object that matches that id", () => {
+      const id = "61857c0154ce63991d588ddc";
+      const action = deleteRobotByIdAction(id);
+      const robots = [
+        {
+          _id: "61855ad154ce63991d588ddb",
+          nombre: "Aire",
+          imagen:
+            "https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/styles/hc_1440x810/public/media/image/2020/04/astro-robot-ps5-1920415.jpg?h=d1cb525d&itok=NpaKRK-Q",
+          caracteristicas: {
+            velocidad: 5,
+            resistencia: 9,
+            creacion: "1996-05-19T22:00:00.000Z",
+          },
+        },
+        {
+          _id: "61857c0154ce63991d588ddc",
+          nombre: "Oreo",
+          imagen:
+            "https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/styles/hc_1440x810/public/media/image/2020/04/astro-robot-ps5-1920415.jpg?h=d1cb525d&itok=NpaKRK-Q",
+          caracteristicas: {
+            velocidad: 3,
+            resistencia: 10,
+            creacion: "1996-05-19T22:00:00.000Z",
+          },
+        },
+      ];
+      const deleteRobot = {
+        _id: "61857c0154ce63991d588ddc",
+        nombre: "Oreo",
+        imagen:
+          "https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/styles/hc_1440x810/public/media/image/2020/04/astro-robot-ps5-1920415.jpg?h=d1cb525d&itok=NpaKRK-Q",
+        caracteristicas: {
+          velocidad: 3,
+          resistencia: 10,
+          creacion: "1996-05-19T22:00:00.000Z",
+        },
+      };
+
+      const newRobots = robotsReducer(robots, action);
+
+      expect(newRobots).not.toContain(deleteRobot);
     });
   });
 });
